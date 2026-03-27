14b89f5b0438140b8c0eb25f3d7bd9a7e45177a4
epashaj <epashaj19@gmail.com>
2026-03-27 22:32:19 +0100

    Destinantion

---
+++ src/modele/Destination.java
@@ -0,0 +1,7 @@
+package modele;
+
+public class Destination extends Case {
+    public boolean estAccesible(){return true;}
+    public char getCaractereBase(){return '.';}
+    public boolean estDestination(){return true;}
+}

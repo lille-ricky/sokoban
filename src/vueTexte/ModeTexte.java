3e695e5271bef85f71439cc041e0da9426842ced
epashaj <epashaj19@gmail.com>
2026-03-28 13:11:18 +0100

    carte, sokobanTexte, modeTexte ini

---
+++ src/vueTexte/ModeTexte.java
@@ -0,0 +1,12 @@
+package vueTexte;
+
+import modele.Carte;
+import modele.Direction;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ModeTexte {
+    private Carte modele;
+    
+}
---
+++ src/vueTexte/SokobanTexte.java
@@ -0,0 +1,20 @@
+package vueTexte;
+
+public class SokobanTexte {
+
+    public static void main(String[] args) {
+        int niveau = 1;
+        if (args.length > 0){
+            try {
+                niveau = Integer.parseInt(args[0]);
+            } catch (NumberFormatException e) {
+        }
+
+        }
+
+        String fichier = "maps/map" + niveau + ".txt";
+        ModeTexte jeu = new ModeTexte(fichier, niveau);
+
+    }
+
+}

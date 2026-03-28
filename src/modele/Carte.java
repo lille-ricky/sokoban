dcd71b0abf259d95743868d62cbea206b06d7ca7
epashaj <epashaj19@gmail.com>
2026-03-28 14:09:14 +0100

    methode construire, Carte.java

--- src/modele/Carte.java
+++ src/modele/Carte.java
@@ -22,5 +22,52 @@ public class Carte {
         construire(lignes);
     }
 
+    private void construire(List<String> lignes){
+        nbLignes = lignes.size();
+        nbColonnes = lignes.stream().mapToInt(String::length).max().orElse(0);
+        grille = new Case[nbLignes][nbColonnes];
+        destinations = new ArrayList<>();
+        nbMouvements = 0;
+
+        for (int y = 0; y < nbLignes; y++){
+            String ligne = lignes.get(y);
+            for (int x = 0; x < nbColonnes; x++){
+                char c = x < ligne.length() ? ligne.charAt(x) : '/';
+                switch(c) {
+                    case '#':
+                        grille[y][x] = new Mur(); break;
+                    case '/':
+                        grille[y][x] = new Vide(); break;
+                    case '.':
+                        grille[y][x] = new Destination(); 
+                        destinations.add(new int[]{x, y}); 
+                        break;
+                    case '@':
+                        grille[y][x] = new Sol();
+                        robot = new Robot(x, y);
+                        grille[y][x].setjoueur(true);
+                        break;
+                    case '+':
+                        grille[y][x] = new Destination();
+                        destinations.add(new int[]{x, y});
+                        robot = new Robot(x, y);
+                        grille[y][x].setjoueur(true);
+                        break;
+                    case '$':
+                        grille[y][x] = new Sol();
+                        grille[y][x].setCaisse(true);
+                        break;
+                    case '*':
+                        grille[y][x] = new Destination();
+                        destinations.add(new int[]{x, y});
+                        grille[y][x].setCaisse(true);
+                        break;
+                    default :
+                        grille[y][x] = new Sol(); break;
+
+                }
+            }
+        }
+    }
     
 }
